97a9d1b86bdb25424be6004c0b42001e153a809d
Gaurishh <98723006+Gaurishh@users.noreply.github.com>
2023-12-24 00:42:22 +0530

    Add files via upload

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,5 +1,26 @@
 import axios from "axios";
 
+export const filterProducts=(searchkey , category)=>async dispatch=>{
+  
+    dispatch({type:'GET_PRODUCTS_REQUEST'})
+
+    try {
+        var filteredProducts = '';
+        const response = await axios.get('https://bakers-nest.onrender.com/api/products/getallproducts')
+        var lowerSearchKey = searchkey.toLowerCase()
+        filteredProducts = response.data.filter(product=>product.name.toLowerCase().includes(lowerSearchKey))
+
+        if(category!='All'){
+            filteredProducts = filteredProducts.filter(product=>product.category==category)            
+        }
+        dispatch({type:'GET_PRODUCTS_SUCCESS' , payload : filteredProducts})
+    } catch (error) {
+        console.log(error)
+        dispatch({type:'GET_PRODUCTS_FAILED' , payload : error})
+    }
+
+}
+
 export const getAllProducts = () => async dispatch => {
 
     dispatch({type: 'GET_PRODUCTS_REQUEST'});
