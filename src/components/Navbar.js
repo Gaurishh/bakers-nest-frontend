25970fc18e0893adbb5d89faf503cbe1007f894f
Gaurishh <98723006+Gaurishh@users.noreply.github.com>
2023-12-26 15:24:53 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,20 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from "react-router-dom";  // Import Link from react-router-dom
+import Filter from "../components/Filter.js";
 
 const Navbar = () => {
-
-  const {user, isAuthenticated, logout, isLoading, loginWithRedirect} = useAuth0();
-
+  const { user, isAuthenticated, logout, isLoading, loginWithRedirect } = useAuth0();
   const cartState = useSelector((state) => state.cartReducer);
 
   return (
     <div>
       <nav className="navbar navbar-expand-lg shadow-lg p-3 mb-5 bg-white rounded navbar-font">
-        <a style={{textDecoration: "none", color: "black"}} href="/shop">
-          <img className="navbar-logo " src="https://i.imgur.com/o3dRG0z.jpg" />
+        <Link to="/shop" style={{ textDecoration: "none", color: "black" }}>
+          <img className="navbar-logo " src="https://i.imgur.com/o3dRG0z.jpg" alt="Baker's Nest Logo" />
           Baker's Nest
-        </a>
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -27,43 +27,47 @@ const Navbar = () => {
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        {isLoading ? <div className="text-center ms-auto">Loading...</div> : <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            {isAuthenticated ? (
-              <div className="dropdown">
-                <a
-                  className="dropdown-toggle nav-link"
-                  type="button"
-                  id="dropdownMenuButton"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  {user["given_name"]}
-                </a>
-                <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  <a className="dropdown-item btn-navbar" href="/shop/orders">
-                    Orders
-                  </a>
-                  <a className="dropdown-item btn-navbar" href="/shop" onClick={()=>{logout()}}>
-                    Logout
-                  </a>
+        {isLoading ? (
+          <div className="text-center ms-auto">Loading...</div>
+        ) : (
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              {isAuthenticated ? (
+                <div className="dropdown">
+                  <Link
+                    className="dropdown-toggle nav-link"
+                    type="button"
+                    id="dropdownMenuButton"
+                    data-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    {user["given_name"]}
+                  </Link>
+                  <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <Link className="dropdown-item btn-navbar" to="/shop/orders">
+                      Orders
+                    </Link>
+                    <Link className="dropdown-item btn-navbar" to="/shop" onClick={() => { logout() }}>
+                      Logout
+                    </Link>
+                  </div>
                 </div>
-              </div>
-            ) : (
+              ) : (
+                <li className="nav-item">
+                  <Link className="nav-link btn-navbar" to="/shop" onClick={() => loginWithRedirect()}>
+                    Login
+                  </Link>
+                </li>
+              )}
               <li className="nav-item">
-                <a className="nav-link btn-navbar" href="/shop" onClick={() => loginWithRedirect()}>
-                  Login
-                </a>
+                <Link className="nav-link btn-navbar" to="/shop/cart">
+                  Cart {cartState.cartItems.length}
+                </Link>
               </li>
-            )}
-            <li className="nav-item">
-              <a className="nav-link btn-navbar" href="/shop/cart">
-                Cart {cartState.cartItems.length}
-              </a>
-            </li>
-          </ul>
-        </div>}
+            </ul>
+          </div>
+        )}
       </nav>
     </div>
   );
